d04ff58d6875788a764ac9d8fb02b13fb0f9c81a
KJF <farrellkj2@gmail.com>
2019-04-08 09:47:40 -0400

    finish data pull test script

--- Elizabeth_temps/test.R
+++ Elizabeth_temps/test.R
@@ -2,46 +2,33 @@
 library(tidyverse)
 library(lubridate)
 
-# Manually read in 1 file (target format)
-goal <- read_csv("./Elizabeth_temps/20190330_FormatedHOBOFloats.csv") 
+# Read in HOBO-treatment ID combinations
+trts <- read_csv('./Elizabeth_temps/HOBO_list.csv')
 
 # Pull in all raw CSV files at once ####
-
-# Define the path where all the raw files are
-path <- "Elizabeth_temps/HOBO_20190330_Bottom8Tanks/Orginial Data_Do Not touch"
-
-# Pull in the names of all raw files
-files <- dir(path, pattern = "*.csv")
-
-# Read in all files from the path
-raw <- tibble(filename = files) %>%
-  mutate(file_contents= map(filename, 
-                            ~ read_csv(file.path(path, .)))) %>%
-  separate(filename, c('SN','SerialNum', 'Date'), sep = " ") %>%
-  select(SerialNum, Date, file_contents)
-
-
-tbl <- list.files(path = path, pattern = "*.csv", full.names=TRUE) %>% 
-  map_df(~read_csv(., skip=2))
-
-
-
-####################
-raw_dir <- paste("FieldData/",Site,sep="")
-out_dir <- "Ch2Model"
-
-
-## LOAD RAW DATA ------------
-# get file names: 
-pattern = "*.csv" # for identifying files to read
-files <- list.files(path, pattern=pattern, full.names=TRUE)
-
-# load data into lists
-read_and_label <- function(x,...){
-  z <- fread(x,...)
-  
-  # add file name without the extension as id column
-  pattern <- "(.*\\/)([^.]+)(\\.csv$)"
-  z$ids <- sub(pattern, "\\2", x)
-  z
+# Set a function to pull in file name (serial number) during read_csv
+read_plus <- function(flnm) {
+  read_csv(flnm, skip=2) %>% 
+    mutate(filename = flnm) %>% 
+    separate(filename, into = c('junk','HOBO_Number'), sep = ' ', remove=T) %>% 
+    mutate(HOBO_Number = as.numeric(HOBO_Number))
 }
+
+# Define the folder path where all the raw files are
+path <- "./Elizabeth_temps/HOBO_20190330_Bottom8Tanks/Orginial Data_Do Not touch/"
+setwd(path)
+
+# Read in all .csv files within the folder 
+data <- list.files(pattern="*.csv", full.names = T) %>% 
+  map_df(~read_plus(.)) %>% 
+  rename(Date_Time = "Date Time - GMT -05:00", Temperature_C = "Temp, (*C)") %>% 
+  mutate(Date_Time = ymd_hms(Date_Time), Date = as.Date(Date_Time), 
+         Time = paste(hour(Date_Time),minute(Date_Time), sep=":")) %>% 
+  left_join(., trts) %>% 
+  select(Date_Time, Date, Time, HOBO_Number, Tank_ID, Treatment, Location, 
+         Temperature_C, "Host Connect", EOF)
+
+# Quick plot to check temperatures ####
+ggplot(data, aes(x = Date_Time, y = Temperature_C, col = Treatment)) + 
+  geom_line() +
+  facet_wrap(Treatment ~ Tank_ID, ncol=2)
